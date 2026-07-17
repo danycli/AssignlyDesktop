7d547252b673c55c6bfafe76bbd6c2ddc769978a
danycli <angelgaming331@gmail.com>
2026-07-18 03:46:37 +0500

    UI Improvements

--- src/main/java/com/assignly/Main.java
+++ src/main/java/com/assignly/Main.java
@@ -16,6 +16,7 @@ public class Main extends Application {
     public void start(Stage stage) {
         DatabaseManager databaseManager = new DatabaseManager(com.assignly.util.AppDirectoryHelper.getDatabaseUrl());
         databaseManager.initializeSchema();
+        com.assignly.util.AnalyticsManager.initialize();
 
         context = new AppContext(
             stage,
